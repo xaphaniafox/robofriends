120a9ca2c8da6aa54cf6eee5d55d9e6ead012f9a
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-17 21:01:19 +0330

    adding some style

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const SearchBox = () => {
   return (
-    <input type='search' placeholder='search robot' />
+    <div className="pa2">
+      <input 
+        className="pa3 ba b--green bg-lightest-blue"
+        type='search' 
+        placeholder='search robot' 
+      />
+    </div>
   )
 }
 
