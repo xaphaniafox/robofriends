9bcaf153c66ddb38adb66fa78be26c33f08564bf
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-17 10:58:46 +0330

    adding div, p tags for future Card (to practice)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import React from 'react';
 
 class App extends React.Component {
   render () {
     return (
-      <h1>Hello World</h1>
+      <div className='f1 tc'>
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
     );
-    }
+  }
 }
 
 export default App;
