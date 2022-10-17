14e3431a850ee681d98bca7e5eec012289a2512f
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-17 11:17:56 +0330

    adding two more cards to the index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import 'tachyons';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <>
+      <Card />
+      <Card />
+      <Card />
+    </>
   </React.StrictMode>
 );
 
