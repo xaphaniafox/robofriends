3eeb306556cdd46858d6e5422e29b683ba8d93d6
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-18 21:16:32 +0330

    just changing the position of the componentDidMount()

--- src/App.js
+++ src/App.js
@@ -11,26 +11,34 @@ class App extends Component {
       searchfield: ''
     }
   }
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value })
-  }
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return (robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-    })
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots}/>
-      </div>
-    )
-  }   
+
   componentDidMount() {
     fetch('https://jsonplaceholder.cypress.io/users')
       .then(response => response.json())
       .then(users => {this.setState({ robots: users })})
   }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return (robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots}/>
+        </div>
+      )
+    }
+    
+  }   
 }
 
 export default App;
