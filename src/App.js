d7495aa0fe8b38990086afbb19c1dce4af21fce9
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-18 13:43:53 +0330

    some styling to look better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -15,17 +15,22 @@ class App extends Component {
     this.setState({searchfield: event.target.value })
   }
   render() {
-    const filteredRobots = robots.filter(robot => {
+    const filteredRobots = this.state.robots.filter(robot => {
       return (robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
     })
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots}/>
       </div>
     )
   }   
+  componentDidMount() {
+    fetch('https://jsonplaceholder.cypress.io/users')
+      .then(response => response.json())
+      .then(users => {this.setState({ robots: users })})
+  }
 }
 
 export default App;
