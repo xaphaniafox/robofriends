7e80d80b40c0a941e735aae8a62e51b9ae47fb7e
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-10-18 12:10:36 +0330

    defining setState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value })
+  }
+  render() {
+    const filteredRobots = robots.filter(robot => {
+      return (robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+    })
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots}/>
+      </div>
+    )
+  }   
 }
 
 export default App;
